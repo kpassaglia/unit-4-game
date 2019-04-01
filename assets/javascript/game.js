8844eb6df7d2900bc40d592aa69753cc4ff0a544
kpassaglia <kpassaglia@gmail.com>
2019-04-01 10:10:33 -0500

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+
+
+
+
+///Random number assigned at the start of the game 
+
+
+
+///crystals assigned random numbers
+
+// on click events for each button (jquery)
+
+//total score captured on the bottom 
+
+//appends to 
+
+//wins/losses print 
+
+//reset buttons 
